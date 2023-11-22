c8b5ade3d06c4c4caa594512998d6ff0f14b8076
Prakhar Tiwari <65062036+prakhartiwari0@users.noreply.github.com>
2023-11-22 23:45:10 +0530

    fix: Remove all frames
    all_Frames true was blocking the selector to start
    again and again after being closed
    in the same tab

--- src/background/background.js
+++ src/background/background.js
@@ -36,7 +36,7 @@ const bg = {
         //     files: ["../scripts/styles/root.css", "../scripts/styles/keySelector.css", "../scripts/styles/elementSelector.css"],     // Specify the CSS file(s) to inject
         // })
         chrome.scripting.executeScript({
-            target: { tabId: tabID,  allFrames: true },
+            target: { tabId: tabID},
             files: [setter]
         })
         chrome.tabs.sendMessage(tabID, { action: "turnOnSelector" });
